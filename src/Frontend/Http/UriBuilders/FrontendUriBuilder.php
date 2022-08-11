43c36cf9ee49ca3693246cfc469209e4098fe729
Christian Ramelow <info@codekandis.net>
2022-08-11 10:41:59 +0200

    Adds a frontend URI builder.

---
+++ src/Frontend/Http/UriBuilders/FrontendUriBuilder.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\MinecraftManager\Frontend\Http\UriBuilders;
+
+use CodeKandis\Tiphy\Http\UriBuilders\AbstractUriBuilder;
+
+/**
+ * Represents a frontend URI builder.
+ * @package codekandis/minecraft-manager
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class FrontendUriBuilder extends AbstractUriBuilder implements FrontendUriBuilderInterface
+{
+}
---
+++ src/Frontend/Http/UriBuilders/FrontendUriBuilderInterface.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\MinecraftManager\Frontend\Http\UriBuilders;
+
+/**
+ * Represents the interface of any frontend URI builder.
+ * @package codekandis/minecraft-manager
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface FrontendUriBuilderInterface
+{
+}
