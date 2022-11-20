45152836d47ba440cb1292b1235dcac9616cea34
Christian Ramelow <info@codekandis.net>
2022-11-20 15:39:30 +0100

    Enhances the `Generator` prototype.

--- public/assets/js/library/Bootstrap.js
+++ public/assets/js/library/Bootstrap.js
@@ -2,5 +2,6 @@
 
 import '../library/Dom/Element.js';
 import '../library/Types/Array.js';
+import '../library/Types/Generator.js';
 import '../library/Types/Object.js';
 import '../library/Types/String.js';
