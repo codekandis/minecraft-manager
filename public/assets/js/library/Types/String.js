54f1d1198a1201958eef04705341c6ed79874c96
Christian Ramelow <info@codekandis.net>
2022-11-27 22:39:39 +0100

    Adds several `String.trim???()` methods.

--- public/assets/js/library/Types/String.js
+++ public/assets/js/library/Types/String.js
@@ -4,6 +4,58 @@ import InvalidIndexException from './InvalidIndexException.js';
 
 String.empty = '';
 
+String.prototype.trimCharacters = function ( characters )
+{
+	const preparedCharacters = [
+		...new Set(
+			characters.split( String.empty )
+		)
+	]
+		.join( '|' );
+
+	return this
+		.replace(
+			new RegExp( String.format`^(${ 0 })+`( preparedCharacters ) ),
+			String.empty
+		)
+		.replace(
+			new RegExp( String.format`(${ 0 })+$`( preparedCharacters ) ),
+			String.empty
+		);
+};
+
+String.prototype.trimCharactersFromStart = function ( characters )
+{
+	const preparedCharacters = [
+		...new Set(
+			characters.split( String.empty )
+		)
+	]
+		.join( '|' );
+
+	return this
+		.replace(
+			new RegExp( String.format`^(${ 0 })+`( preparedCharacters ) ),
+			String.empty
+		);
+};
+
+String.prototype.trimCharactersFromEnd = function ( characters )
+{
+	const preparedCharacters = [
+		...new Set(
+			characters.split( String.empty )
+		)
+	]
+		.join( '|' );
+
+	return this
+		.replace(
+			new RegExp( String.format`(${ 0 })+$`( preparedCharacters ) ),
+			String.empty
+		);
+};
+
 String.format = function ( template, ...placeholders )
 {
 	return (
