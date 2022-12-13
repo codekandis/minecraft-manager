2e1b3939bd5d8dc5e175cc361c9380ea635393b7
Christian Ramelow <info@codekandis.net>
2022-12-13 09:43:24 +0100

    Adds the `Headers::map()` method.

--- public/assets/js/library/Bootstrap.js
+++ public/assets/js/library/Bootstrap.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import '../library/Dom/Element.js';
+import '../library/Net/FileList.js';
+import '../library/Net/Http/Headers/Headers.js';
 import '../library/Net/Url.js';
 import '../library/Types/Array.js';
 import '../library/Types/Generator.js';
