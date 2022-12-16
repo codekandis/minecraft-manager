554d93ac56aef36d0062778e0fc3c45d0b65edba
Christian Ramelow <info@codekandis.net>
2022-12-17 00:35:39 +0100

    Fixes the user entity property types.

--- src/Environment/Entities/EntityPropertyMappings/UserEntityPropertyMappings.php
+++ src/Environment/Entities/EntityPropertyMappings/UserEntityPropertyMappings.php
@@ -1,10 +1,11 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\MinecraftManager\Environment\Entities\EntityPropertyMappings;
 
+use CodeKandis\Converters\BiDirectionalConverters\BoolToIntStringBiDirectionalConverter;
 use CodeKandis\Entities\EntityPropertyMappings\EntityPropertyMapping;
 
 /**
- * Represents the entity property mappings of the order entity.
+ * Represents the entity property mappings of the user entity.
  * @package codekandis/minecraft-manager
  * @author Christian Ramelow <info@codekandis.net>
  */
@@ -16,7 +17,7 @@ class UserEntityPropertyMappings extends AbstractPersistableEntityPropertyMappin
 	public function __construct()
 	{
 		parent::__construct(
-			new EntityPropertyMapping( 'isActive', null ),
+			new EntityPropertyMapping( 'isActive', new BoolToIntStringBiDirectionalConverter() ),
 			new EntityPropertyMapping( 'name', null ),
 			new EntityPropertyMapping( 'eMail', null ),
 			new EntityPropertyMapping( 'password', null )
