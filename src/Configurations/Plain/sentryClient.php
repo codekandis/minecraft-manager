1ad6bfdbdfb7da062f6aa9f7860c45fec3665f43
Christian Ramelow <info@codekandis.net>
2023-08-07 11:21:43 +0200

    Updates the release version to `0.7.0`.

--- src/Configurations/Plain/sentryClient.php
+++ src/Configurations/Plain/sentryClient.php
@@ -8,6 +8,6 @@ return [
 	'displayErrors' => false,
 	'errorTypes'    => E_ALL,
 	'environment'   => 'production',
-	'release'       => '0.6.1',
+	'release'       => '0.7.0',
 	'serverName'    => 'minecraft-manager.codekandis'
 ];
